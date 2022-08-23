9e93e49826a3aa619623d71626d7b90bc8518a00
azizmenzli <azizmenzli3@gmail.com>
2022-08-23 13:03:04 +0200

    commit

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+$('.content').css( {
+    'position':'absolute',
+    'top':'0',
+    'left':'0',
+    'z-index':'1',
+    'bottom': '0',
+  'background': 'rgba(0, 0, 0, 0.5)',
+  'color': '#f1f1f1',
+  'width': '100%',
+  'padding': '10px',
+  'font-size': '20px',
+})
+$('#myVideo').css( {
+  'position': 'fixed',
+  'right': '0',
+  'bottom': '0',
+  'min-width': '100%', 
+  'min-height': '100%',
+})
+$('.button').css({
+  //'background-color': '#FFEBCD',
+
+'color': 'white',  
+'padding': '20px 60px',
+'text-align': 'center',
+'text-decoration': 'none',  
+'display': 'inline-block',
+'opacity': '0.5'})
 var data=[
 {place:'Hammamet',
 discription:'',
@@ -5,7 +33,7 @@ imgSrc:'',
 imgMpa:''},
 {
 	place:'Sousse',
-	discription:,
+	discription:"",
 	imgSrc,
 	imgMpa
 },
@@ -21,4 +49,4 @@ imgMpa},
 discription,
 imgSrc,
 imgMpa}]
-console.log('hi')
+
